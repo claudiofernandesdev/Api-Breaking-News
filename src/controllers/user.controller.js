5623d186421864c75f6d478f8f427a2bc2eb48c9
Claudiofernandesdev <claudiofernandes.dev@mail.com>
2023-05-22 21:25:01 -0300

    refactor: update route and global middlewares add

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -38,18 +38,30 @@ const findAll = async (req, res) => {
 };
 
 const findById = async (req, res) => {
-  const id = req.params.id;
-
-  if(!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(400).send({ message: "Invalid ID"});
-  }
-
-  const user = await userService.findByIdService(id);
-  if (!user) {
-    return res.status(400).send({ message: "User not found" });
-  }
-
+  const user = req.user;
   res.send(user);
 };
 
-module.exports = { create, findAll, findById };
+const update = async (req, res) => {
+  const { name, username, email, password, avatar, background } = req.body;
+
+  if (!name && !username && !email && !password && !avatar && !background) {
+    res.status(400).send({ message: "Submit at least field for update!" });
+  }
+
+  const { id, user } = req;
+
+  await userService.updateService(
+    id,
+    name,
+    username,
+    email,
+    password,
+    avatar,
+    background
+  );
+
+  res.send({ message: "User successfully updated." });
+};
+
+module.exports = { create, findAll, findById, update };
