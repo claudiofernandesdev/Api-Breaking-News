c6bfbd7fe006894547b8a3ba77abe4298ddf80b3
Claudiofernandesdev <claudiofernandes.dev@mail.com>
2023-05-30 23:28:53 -0300

    feat: pagination add

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -1,9 +1,13 @@
-import { createService, findAllService } from "../services/news.service.js";
+import {
+  createService,
+  findAllService,
+  countNews,
+} from "../services/news.service.js";
 
 const create = async (req, res) => {
   try {
     const { title, text, banner } = req.body;
-    
+
     if (!title || !text || !banner) {
       res.status(400).send({ message: "Submit all fields for registration" });
     }
@@ -23,13 +27,55 @@ const create = async (req, res) => {
 
 const findAll = async (req, res) => {
   try {
-    const news = await findAllService();
+    let { limit, offset } = req.query;
+    limit = Number(limit);
+    offset = Number(offset);
+
+    if (!limit) {
+      limit = 5;
+    }
+
+    if (!offset) {
+      offset = 0;
+    }
+
+    const news = await findAllService(offset, limit);
+    const total = await countNews();
+    const currentUrl = req.baseUrl;
+
+    const next = offset + limit;
+    const nextUrl =
+      next < total ? `${currentUrl}?limit=${limit}&offset=${next}` : null;
+
+    const previous = offset - limit < 0 ? null : offset - limit;
+    const previousUrl =
+      previous != null
+        ? `${currentUrl}?limit=${limit}&offset=${previous}`
+        : null;
 
     if (news.length === 0) {
       return res.status(400).send({ message: "There are no registered news!" });
     }
 
-    res.send(news);
+    res.send({
+      nextUrl,
+      previousUrl,
+      limit,
+      offset,
+      total,
+
+      results: news.map(item => ({
+        id: item._id,
+        title: item.title,
+        text: item.text,
+        banner: item.banner,
+        likes: item.likes,
+        comments: item.comments,
+        name: item.user.name,
+        userName: item.user.username,
+        userAvatar: item.user.avatar
+      }))
+    });
   } catch (err) {
     res.status(500).send(err.message);
   }
--- src/models/News.js
+++ src/models/News.js
@@ -26,7 +26,7 @@ const NewsSchema = new mongoose.Schema({
     type: Array,
     required: true,
   },
-  coments: {
+  comments: {
     type: Array,
     required: true,
   },
