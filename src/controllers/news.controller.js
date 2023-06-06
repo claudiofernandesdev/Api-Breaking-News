1ebda57106a7c2c3c9f751086df1375e3fb7d2b5
Claudiofernandesdev <claudiofernandes.dev@mail.com>
2023-06-05 22:01:37 -0300

    feat: news like add

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -8,6 +8,8 @@ import {
   byUserService,
   updateService,
   eraseService,
+  likeNewsService,
+  deleteLikeNewsService,
 } from "../services/news.service.js";
 
 export const create = async (req, res) => {
@@ -224,7 +226,7 @@ export const update = async (req, res) => {
 export const erase = async (req, res) => {
   try {
     const { id } = req.params;
-    
+
     const news = await findByIdService(id);
 
     if (news.user.id != req.userId) {
@@ -234,6 +236,24 @@ export const erase = async (req, res) => {
     await eraseService(id);
 
     return res.send({ message: "News deleted successfully!" });
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+};
+
+export const likeNews = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const userId = req.userId;
+
+    const newsLiked = await likeNewsService(id, userId);
+
+    if (!newsLiked) {
+      await deleteLikeNewsService(id, userId);
+      return res.status(200).send({message: "Like successfully removed!"})
+    }
+
+    res.status(200).send({ message: "Like done successfully!" });
   } catch (err) {
     res.status(500).send({ message: err.message });
   }
--- src/services/news.service.js
+++ src/services/news.service.js
@@ -27,4 +27,13 @@ export const updateService = (id, title, text, banner) =>
     { rawResult: true }
   );
 
-export const eraseService = (id) => News.findByIdAndDelete({_id: id});
+export const eraseService = (id) => News.findByIdAndDelete({ _id: id });
+
+export const likeNewsService = (idNews, userId) =>
+  News.findOneAndUpdate(
+    { _id: idNews, "likes.userId": { $nin: [userId] } },
+    { $push: { likes: { userId, created: new Date() } } }
+  );
+
+export const deleteLikeNewsService = (idNews, userId) =>
+  News.findOneAndUpdate({ _id: idNews }, { $pull: { likes: { userId } } });
