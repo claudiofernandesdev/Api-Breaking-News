a5c713bd0b5087fcb586c9097abf948a3751ce9f
Claudiofernandesdev <claudiofernandes.dev@mail.com>
2023-07-10 15:05:31 -0300

    refact swagger route to cjs module

---
+++ src/routes/swagger.route.cjs
@@ -0,0 +1,9 @@
+const router = require('express').Router;
+const swaggerUi = require("swagger-ui-express");
+
+const swaggerDocument =  require ("../swagger.json");
+
+swaggerRouter.use("/", swaggerUi.serve);
+swaggerRouter.get("/", swaggerUi.setup(swaggerDocument));
+
+export default swaggerRouter;
